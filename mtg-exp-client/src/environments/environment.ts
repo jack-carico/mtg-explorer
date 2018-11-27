41f271df685feeabbfda0d46c232ceb88c3cd845
Jack Carico <jack.carico@gmail.com>
2018-11-26 22:53:32 -0800

    setting up base framework

--- mtg-exp-client/src/environments/environment.ts
+++ mtg-exp-client/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  cardIndexUrl: 'http://localhost:3737/my-card-index.json',
+  setIndexUrl: 'http://localhost:3737/all-sets.json'
 };
 
 /*
